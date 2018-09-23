7eaac13a585cd1bb312f2ddf333d562678a5b5d8
Theodore <teohaik@gmail.com>
2018-09-23 23:03:13 +0300

    created onClickListener

--- app/src/main/java/gr/uom/android/buttoncounter/MainActivity.java
+++ app/src/main/java/gr/uom/android/buttoncounter/MainActivity.java
@@ -1,13 +1,36 @@
 package gr.uom.android.buttoncounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  EditText userInput;
+    private Button button;
+    private TextView textView;
+    private int numTimesClicked =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText)findViewById(R.id.editText);
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textView);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numTimesClicked++;
+                String result = "\nThe button clicked " + numTimesClicked + " times";
+                textView.append(result);
+            }
+        };
+        button.setOnClickListener(onClickListener);
     }
 }
